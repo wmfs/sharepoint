6bd8bc5c16ede9aaa71573bba5fb49bce839103e
Meena.Hoda <meena.hoda@wmfs.net>
2018-11-07 08:12:28 +0000

    refactored out functions to use promises

--- src/index.js
+++ src/index.js
@@ -3,47 +3,62 @@ const dotEnv = require('dotenv')
 const axios = require('axios')
 dotEnv.config() // Allows use of .env file for setting env variables, if preferred.
 
-console.log('SharePoint Authentication')
-console.log('-------------------------')
+module.exports = async function () {
+  console.log('SharePoint Authentication')
+  console.log('-------------------------')
 
-// Grab URL from env variables
-const url = process.env.SHAREPOINT_URL
-console.log(`SharePoint URL: ${url}`)
+  // Grab URL from env variables
+  const url = process.env.SHAREPOINT_URL
+  console.log(`SharePoint URL: ${url}`)
 
-// Grab credentials from env variables
-const credentials = {
-  username: process.env.SHAREPOINT_USERNAME,
-  password: process.env.SHAREPOINT_PASSWORD
-}
-console.log(`Username: ${credentials.username}`)
+  // Grab credentials from env variables
+  const credentials = {
+    username: process.env.SHAREPOINT_USERNAME,
+    password: process.env.SHAREPOINT_PASSWORD
+  }
+  console.log(`Username: ${credentials.username}`)
+  
+  try {
+    const { headers } = await getAuth(url, credentials)
 
-// Authenticate credentials with SharePoint
-spauth.getAuth(url, credentials)
-  .then(data => {
-    // Success!
-    const headers = data.headers
     console.log(`Got cookie? ${headers.hasOwnProperty('Cookie')}`)
     console.log(`Auth type: ${headers.Cookie.split('=')[0]}`)
+    
     headers['Accept'] = 'application/json;odata=verbose'
+
     // Call the web endpoint
-    axios.get(
-      url + '/_api/web',
-      {
-        headers: headers,
-        responseType: 'json'
-      }
-    ).then(response => {
-      const site = response.data.d
-      console.log('')
-      console.log('Site details')
-      console.log('------------')
-      console.log(`ID: ${site.Id}`)
-      console.log(`Title: ${site.Title}`)
-      console.log(`Description: ${site.Description}`)
-      console.log(`Created: ${site.Created}`)
-      console.log(`Modified: ${site.LastItemUserModifiedDate}`)
-      console.log('')
-    }).catch(err => {
-      console.error(err)
-    })
+    const { data } = await getWebEndpoint(`${url}/_api/web`, { headers, responseType: 'json' })
+
+    const site = data.d
+    
+    console.log('')
+    console.log('Site details')
+    console.log('------------')
+    console.log(`ID: ${site.Id}`)
+    console.log(`Title: ${site.Title}`)
+    console.log(`Description: ${site.Description}`)
+    console.log(`Created: ${site.Created}`)
+    console.log(`Modified: ${site.LastItemUserModifiedDate}`)
+    console.log('')
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+const getAuth = (url, credentials) => {
+  return new Promise((resolve, reject) => {
+    spauth
+    .getAuth(url, credentials)
+    .then(response => resolve(response))
+    .catch(err => reject(err))
   })
+}
+
+const getWebEndpoint = (url, config) => {
+  return new Promise ((resolve, reject) => {
+    axios
+    .get(url, config)
+    .then(response => resolve(response))
+    .catch(err => reject(err))
+  })
+}
