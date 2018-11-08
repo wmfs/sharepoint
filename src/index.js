8fe3738557db6e0e01274bacddf5e2b934757eea
Tim Needham <tim.needham@wmfs.net>
2018-11-08 16:58:51 +0000

    Get digest value and create folders.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,16 @@ const dotEnv = require('dotenv')
 const axios = require('axios')
 dotEnv.config() // Allows use of .env file for setting env variables, if preferred.
 
-const SECURITY_TOKEN_REQUEST_ENVELOPE = '<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://www.w3.org/2005/08/addressing\">' + 
-'<s:Header><a:Action s:mustUnderstand=\"1\">http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</a:Action>' + 
-'<a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo>' + 
-'<a:To s:mustUnderstand=\"1\">https://login.microsoftonline.com/extSTS.srf</a:To>' + 
-'<o:Security s:mustUnderstand=\"1\" xmlns:o=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\">' + 
-'<o:UsernameToken><o:Username>[USERNAME]</o:Username><o:Password>[PASSWORD]</o:Password></o:UsernameToken>' + 
-'</o:Security></s:Header><s:Body><t:RequestSecurityToken xmlns:t=\"http://schemas.xmlsoap.org/ws/2005/02/trust\">' + 
-'<wsp:AppliesTo xmlns:wsp=\"http://schemas.xmlsoap.org/ws/2004/09/policy\"><a:EndpointReference>' + 
-'<a:Address>[SITE]</a:Address></a:EndpointReference></wsp:AppliesTo>' + 
-'<t:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</t:KeyType>' + 
-'<t:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</t:RequestType>' + 
-'<t:TokenType>urn:oasis:names:tc:SAML:1.0:assertion</t:TokenType>' + 
-'</t:RequestSecurityToken></s:Body></s:Envelope>'
-
 module.exports = async function () {
   console.log('SharePoint Authentication')
   console.log('-------------------------')
 
   // Grab URL from env variables
   const url = process.env.SHAREPOINT_URL
-  console.log(`SharePoint URL: ${url}`)
+  console.log(`SharePoint URL: '${url}'`)
+
+  const rootSharePointDirPath = process.env.SHAREPOINT_DIR_PATH
+  console.log(`rootSharePointDirPath: '${rootSharePointDirPath}'`)
 
   // Grab credentials from env variables
   const credentials = {
@@ -31,20 +20,20 @@ module.exports = async function () {
     password: process.env.SHAREPOINT_PASSWORD
   }
   console.log(`Username: ${credentials.username}`)
-  
+
   try {
-    const { headers } = await spauth.getAuth(url, credentials)
+    const {headers} = await spauth.getAuth(url, credentials)
 
     console.log(`Got cookie? ${headers.hasOwnProperty('Cookie')}`)
     console.log(`Auth type: ${headers.Cookie.split('=')[0]}`)
-    
+
     headers['Accept'] = 'application/json;odata=verbose'
 
     // Call the web endpoint
-    const { data } = await axios.get(`${url}/_api/web`, { headers, responseType: 'json' })
+    const {data} = await axios.get(`${url}/_api/web`, {headers, responseType: 'json'})
 
     const site = data.d
-    
+    const ServerRelativeUrl = site.ServerRelativeUrl
     console.log('')
     console.log('Site details')
     console.log('------------')
@@ -52,61 +41,51 @@ module.exports = async function () {
     console.log(`Title: ${site.Title}`)
     console.log(`Description: ${site.Description}`)
     console.log(`Created: ${site.Created}`)
+    console.log(`ServerRelativeUrl: ${ServerRelativeUrl}`)
     console.log(`Modified: ${site.LastItemUserModifiedDate}`)
     console.log('')
 
-    const securityToken = SECURITY_TOKEN_REQUEST_ENVELOPE
-      .replace('[USERNAME]', credentials.username)
-      .replace('[PASSWORD]', credentials.password)
-      .replace('[SITE]', process.env.SHAREPOINT_SITE)
+    // Get a "form digest value" (time limited)
+    const formDigestValue = await getFormDigestValue(url, headers)
+    console.log(`formDigestValue: ${formDigestValue}`)
+    console.log('')
 
-    // get access token
-    const securityTokenRes = await axios.post(
-      process.env.SECURITY_TOKEN_URL,
-      securityToken,
-      { headers, responseType: 'json' }
-    )
+    // Create a new folder
+    const createFolderResponse = await createAFolder('Hello', url, formDigestValue, headers)
+    console.log(createFolderResponse)
+    console.log('')
+    console.log(`Created folder './${createFolderResponse.Name}' at ${createFolderResponse.TimeCreated} (UniqueId=${createFolderResponse.UniqueId})`)
+    console.log('')
 
-    console.log('>>', securityTokenRes.data)
-
-    // Get context info
-    // const contextInfo = await axios.post(`${url}/_api/contextinfo`, { headers, responseType: 'json' })
-    // console.log('>>>', contextInfo)
-
-    // Get the contents
-    // const contents = await getContents(url, headers)
-    // console.log('Contents:', contents.map(i => i.Name).join(', '))
-    // console.log('')
-
-    // Attempt to create a folder
-    // const createFolderRes = await axios({
-    //   method: 'post',
-    //   url: `${url}/_api/web/Folders`,
-    //   header: {
-    //     ...headers,
-    //     'X-RequestDigest': 'form digest value',
-    //     'content-type': 'application/json;odata=verbose'
-    //   },
-    //   responseType: 'json',
-    //   data: {
-    //     '__metadata': { 'type': 'SP.Folder' },
-    //     'ServerRelativeUrl': '/Shared Documents/General/TYMLYNODE/NewTestFolder'
-    //   }
-    // })
-
+    // Get file contents
+    const contents = await getContents(url, headers)
+    console.log('Contents:\n', contents.map(i => i.Name).join('\n'))
   } catch (e) {
     if (e.response) {
-      console.log(`${e.response.status}: ${e.response.statusText}`) 
+      console.log(`${e.response.status}: ${e.response.statusText}`)
       console.log(JSON.stringify(e.response.data))
     } else console.error(e)
   }
+}
+
+const getFormDigestValue = async (url, headers) => {
+  const contextInfo = await axios({
+    method: 'post',
+    url: `${url}/_api/contextinfo`,
+    headers: {
+      ...headers,
+      'content-type': 'application/json;odata=verbose'
+    },
+    responseType: 'json'
+  })
+  return contextInfo.data.d.GetContextWebInformation.FormDigestValue
 }
 
 const getContents = async (url, headers) => {
   const get = type => {
     return axios.get(
       `${url}/_api/web/GetFolderByServerRelativeUrl('${process.env.SHAREPOINT_DIR_PATH}')/${type}`,
-      { headers, responseType: 'json' }
+      {headers, responseType: 'json'}
     )
   }
 
@@ -114,4 +93,23 @@ const getContents = async (url, headers) => {
   const files = await get('Files')
 
   return [...folders.data.d.results, ...files.data.d.results]
+}
+
+// https://docs.microsoft.com/en-us/sharepoint/dev/sp-add-ins/working-with-folders-and-files-with-rest
+const createAFolder = async (folderName, url, formDigestValue, headers) => {
+  const result = await axios({
+    method: 'post',
+    url: `${url}/_api/web/folders`,
+    headers: {
+      ...headers,
+      'content-type': 'application/json;odata=verbose',
+      'X-RequestDigest': formDigestValue
+    },
+    data: {
+      __metadata: {type: 'SP.Folder' },
+      ServerRelativeUrl: `${process.env.SHAREPOINT_DIR_PATH}/${folderName}`
+    },
+    responseType: 'json'
+  })
+  return result.data.d
 }
